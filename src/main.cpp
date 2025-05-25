438ea4ef91c271342f5dd6c154cb56b32fc2cb81
Goof <Byliba50005@mail.ru>
2025-05-25 21:44:33 +0700

    немного изменил main

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include "../include/singleton.h"
 
 void printUsage() {
-    std::cout << "Использование:\n"
-              << "./main encrypt <filename> <password>  - шифрование\n"
-              << "./main decrypt <filename> <password>  - дешифрование\n";
+    std::cout << "Использование:" << std::endl; 
+    std::cout << "./main encrypt <path> <password> - шифрование" << std::endl;
+    std::cout << "./main decrypt <path> <password> - дешифрование" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -14,23 +14,26 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::string command = argv[1];
-    std::string filename = argv[2];
+    std::string mode = argv[1];
+    std::string path = argv[2];
     std::string password = argv[3];
 
-    bool success = false;
+    try {
+        Singleton& singleton = Singleton::getInstance();
+        singleton.setKey(password);
 
-    if (command == "encrypt") {
-        success = Singleton::Instance().encryptFile(filename, password);
-    } else if (command == "decrypt") {
-        success = Singleton::Instance().decryptFile(filename, password);
-    } else {
-        printUsage();
-        return 1;
-    }
-
-    if (!success) {
-        std::cerr << "Операция не выполнена." << std::endl;
+        if (mode == "encrypt") {
+            singleton.encrypt(path);
+            std::cout << "Шифрование завершено" << std::endl;
+        } else if (mode == "decrypt") {
+            singleton.decrypt(path);
+            std::cout << "Дешифрование завершено" << std::endl;
+        } else {
+            printUsage();
+            return 1;
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Ошибка: " << e.what() << std::endl;
         return 1;
     }
 
